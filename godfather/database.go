a34bf46a08c42e4753fc18ee9af18dcda52840e8
Andrey Sploshnov <andrey.sploshnov@kaspersky.com>
2024-10-07 09:34:27 +0200

    Add missed files

--- godfather/database.go
+++ godfather/database.go
@@ -1,37 +1,50 @@
 package godfather
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/labstack/gommon/log"
 )
 
 type Database struct {
-	conn *pgx.Conn
+	handle *sql.DB
 }
 
 func InitDB(host string, port int, user string, passwd string, database string) (*Database, error) {
 	uri := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, passwd, host, port, database)
-	conn, err := pgx.Connect(context.Background(), uri)
+	db, err := sql.Open("pgx", uri)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Database{conn: conn}, nil
+	return &Database{handle: db}, nil
 }
 
 func (db *Database) Migrate() error {
-	sql := `
-	CREATE TABLE IF NOT EXISTS sources (
-		id BIGSERIAL PRIMARY KEY,
-		name VARCHAR NOT NULL,
-		update_interval INTERVAL NOT NULL,
-		last_update TIMESTAMP
-	);
-	CREATE TABLE IF NOT EXISTS moex_operands (
-	);
-	`
-	_, err := db.conn.Exec(context.Background(), sql)
-	return err
+	instance, err := postgres.WithInstance(db.handle, new(postgres.Config))
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance("file://db/migrations", "pgx5", instance)
+	if err != nil {
+		return err
+	}
+
+	err = m.Up()
+	if err != nil {
+		return err
+	}
+
+	version, _, err := m.Version()
+	if err != nil {
+		return err
+	}
+	log.Info(fmt.Sprintf("Database migrated to version %d", version))
+	return nil
 }
--- ui/src/app/sources/sources.component.ts
+++ ui/src/app/sources/sources.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SourcesService, Source } from '../sources.service';
 
 @Component({
   selector: 'app-sources',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sources.component.html',
   styleUrl: './sources.component.scss'
 })
-export class SourcesComponent {
 
+export class SourcesComponent implements OnInit {
+  sources: Source[] = [];
+
+  constructor (private sourcesService: SourcesService) {}
+  ngOnInit(): void {
+    this.getSources();
+  }
+
+  getSources() {
+    this.sourcesService.getSources().subscribe((data: Source[]) => {
+      this.sources = data
+    });
+  }
+
+  addSource() {
+    var newSource : Source = {
+      id: 0,
+      name: '',
+      uri: '',
+      updateInterval: '',
+      lastUpdated: ''
+    };
+    this.sourcesService.addSource(newSource).subscribe(() => {
+      this.getSources();
+    });
+  }
+
+  deleteSource(source: Source) {
+    this.sourcesService.deleteSource(source).subscribe(() => {
+      this.getSources();
+    });
+  }
 }
