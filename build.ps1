2ffb74b485f3766851b079d54586d21fd34173ba
Andrey Sploshnov <ice@ice-castle.ru>
2025-08-09 14:40:36 +0300

    Extract active items only while quering MOEX watchlist in MOEXMON

--- build.ps1
+++
@@ -1,5 +0,0 @@
-# notice how we avoid spaces in $now to avoid quotation hell in go build command
-$now = Get-Date -UFormat "%Y-%m-%d_%T"
-$sha1 = (git rev-parse HEAD).Trim()
-
-go build -o godfather.exe -ldflags "-X main.sha1ver=$sha1 -X main.buildTime=$now" ./cmd/godfather-cmd
